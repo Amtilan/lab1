1c1711c9c89b1d7ce09904f56e37cf70cae4d9e3
Amtilan <149930972+Amtilan@users.noreply.github.com>
2024-02-17 14:12:35 +0600

    Change

--- week_3/TSIS3.2/0-5.py
+++ week_3/TSIS3.2/0-5.py
@@ -1,8 +1,15 @@
 import math
-str = str(input())
+from itertools import permutations
 
-def permutation(str):
-    a = len(str)
+str1 = str(input())
+
+def permutation(str1):
+    a = len(str1)
+    for i in permutations(str1):
+        b = ''
+        for j in i:
+            b += str(j)
+        print(b)
     return math.factorial(a)
 
-print(permutation(str))
+print(permutation(str1))
--- week_3/TSIS3.2/3-1.py
+++ week_3/TSIS3.2/3-1.py
@@ -1,20 +1,30 @@
 import random
 
-name = input("Hello! What is your name?\n")
-
-print( f"Well,{name}, I am thinking of a number between 1 and 20.")
-
-num = random.randint(1, 20)
-numb = 0
-gss = 0
-
-while numb != num:
-        numb = int(input("Take a guess.\n"))
-        gss += 1
-        if numb < num:
+class game:
+    def __init__(self, name, game_count):
+        self.name = name
+        self.game_count = game_count
+        self.gss = int(0)
+        self.numb = int(-1)
+    def get_correct(self):
+        try:
+            return int(input("Take a guess.\n"))
+        except:
+            return print("Please enter integer")
+    def get_checked(self):
+        if self.numb < self.game_count:
             print("Your guess is too low.")
-        elif numb > num:
+        elif self.numb > self.game_count:
             print("Your guess is too high.")
 
+game = game(input("Hello! What is your game?\n"), random.randint(1, 20))
 
-print(f"Good job, {name}! You guessed my number in {gss} guesses!")
+print( f"Well,{game.name}, I am thinking of a number between 1 and 20.")
+
+while game.numb != game.game_count:
+        game.numb = game.get_correct()
+        game.gss += 1
+        game.get_checked()
+
+
+print(f"Good job, {game.name}! You guessed my number in {game.gss} guesses!")
